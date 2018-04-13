bfb54b0a39b5ae2d410ddbb305bd398a980db8eb
Lonnie Souder II <souderl9@students.rowan.edu>
2018-04-13 13:21:43 -0400

    Created Controller struct

---
+++ Code/Controller.h
@@ -0,0 +1,24 @@
+/*
+ * Controller.h
+ *
+ * Lonnie L. Souder II
+ * 04/13/2018
+ *
+ * This file defines a data structure which contains all the necessary
+ * data for a PID controller including gain constants and rolling
+ * sums for error, integral of error, and derivative of error.
+ * In addition, this file will include methods used to derive and manipulate
+ * all of data collected by the PID controller
+ */
+
+struct Controller {
+	// Gain Constants
+	int Kp;
+	int Ki;
+	int Kd;
+
+	// Error Signals
+	int e;		// Current error signal
+	int int_e;	// Integral of previous error signal
+	int de;		// Instantaneous derivative of error signal
+};
